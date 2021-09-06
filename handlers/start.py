7e64405904fa93ec4741abe411f873a7dde4e759
divyanshprakash <shahparvshah@gmail.com>
2021-09-06 17:03:28 +0530

    Update

--- handlers/start.py
+++ handlers/start.py
@@ -64,7 +64,7 @@ async def start_(client: Client, message: Message):
                         "🌐 Wiki's Page", url="https://github.com/levina-lab/veezmusic/wiki/Veez-Music-Wiki's")
                 ],[
                     InlineKeyboardButton(
-                        "🧪 Source Code 🧪", url="https://github.com/levina-lab/VeezMusic"
+                        "🧪 Source Code 🧪", url="https://telegra.ph/Ultroid-09-06-2"
                     )
                 ]
             ]
